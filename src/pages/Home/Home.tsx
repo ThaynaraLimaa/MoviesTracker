f67b587020847f419d99927573963191597e000c
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2024-11-04 11:35:00 -0300

    Wrapped Outlet in the main tag

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,18 @@
-import Button from '../../components/UI/Button'
 import LinkButton from '../../components/UI/LinkButton'
 import styles from './Home.module.css'
 import MoviesList from './MoviesList'
 import Search from './Search'
 
 export default function Home() {
-    const handleClick = () => {
-        alert('This button is not ready yet')
-    }
+   
     return (
-        <main>
+        <>
             <Search />
             <div className={styles.moviesHeader}>
                 <h2>My movies</h2>
-                <LinkButton to='/'>Add movie</LinkButton>
+                <LinkButton to='/addMovie'>Add movie</LinkButton>
             </div>
             <MoviesList />
-        </main>
+        </>
     )
 }
