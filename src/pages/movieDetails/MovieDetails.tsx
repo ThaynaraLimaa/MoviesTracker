36e6a9c4193825557b0bcee0aab11fdc4fe76f39
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2024-11-05 09:49:55 -0300

    Added a success message in Movie Details for when a movie is added

--- src/pages/movieDetails/MovieDetails.tsx
+++ src/pages/movieDetails/MovieDetails.tsx
@@ -2,25 +2,42 @@ import styles from './MovieDetails.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query';
 import { getMovie } from '../../service/fecthMovies';
 import ErrorMessage from '../../components/UI/ErrorMessage';
+import MessageAlert from '../../components/UI/MessageAlert';
+import { useEffect, useState } from 'react';
 
 
 export default function MovieDetails() {
+    const location = useLocation(); 
+    const success = location.state ? location.state.success : false
+    const [showMessage, setShowMessage] = useState<boolean>(success)
     const { id } = useParams();
 
-    const {data: movie, error, isLoading, isError} = useQuery({
+    useEffect(() => {
+        if(success) {
+            setTimeout(() => {
+                setShowMessage(false)
+            }, 2000)
+        } else {
+            setShowMessage(false)
+        }
+    })
+
+    const { data: movie, error, isLoading, isError } = useQuery({
         queryKey: ['movies', id],
         queryFn: () => getMovie(id as string)
     })
 
-    if(isLoading) return <h2>Loading...</h2>
-    if(isError) return <ErrorMessage name={error.name} message={error.message} />
+    if (isLoading) return <h2>Loading...</h2>
+    if (isError) return <ErrorMessage name={error.name} message={error.message} />
 
     return (
-        <div className={styles.movieDetailsContainer}>
+        <>
+        {showMessage && <MessageAlert type='success' message='Testing'/>}
+            <div className={styles.movieDetailsContainer}>
             <div className={styles.left}>
                 <img src={movie?.imageUrl} alt={movie?.title} className={styles.image} />
                 <div className={styles.buttonsContainer}>
@@ -34,5 +51,6 @@ export default function MovieDetails() {
                 <p className={styles.releaseData}> <FontAwesomeIcon icon={faCalendarDays} /> {movie?.releaseDate}</p>
             </div>
         </div>
+        </>
     )
 } 
