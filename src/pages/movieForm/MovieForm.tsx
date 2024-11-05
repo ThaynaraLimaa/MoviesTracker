8120e14bc396bcf2821840c117f610adbb5d51b9
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2024-11-05 09:48:04 -0300

    Implemented dynamic movie creation with form

--- src/pages/movieForm/MovieForm.tsx
+++ src/pages/movieForm/MovieForm.tsx
@@ -1,36 +1,68 @@
 import styles from './MovieForm.module.css'
-import { FormEvent, useRef } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import Input from "../../components/form/Input";
 import Button from "../../components/UI/Button";
 import Textarea from '../../components/form/Textarea';
 import MonthSelect from './MonthSelect';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { postMovie } from '../../service/fecthMovies';
+import { useNavigate } from 'react-router-dom';
+import MessageAlert from '../../components/UI/MessageAlert';
 
 export default function MovieForm() {
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const [showError, setshowError] = useState(false)
+
     const titleRef = useRef<HTMLInputElement>(null);
     const urlRef = useRef<HTMLInputElement>(null);
     const descriptionRef = useRef<HTMLTextAreaElement>(null);
     const releaseMonthRef = useRef<HTMLSelectElement>(null);
     const releaseYearRef = useRef<HTMLInputElement>(null);
 
+    const { error, mutate } = useMutation({
+        mutationFn: postMovie,
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['movies'] })
+            navigate(`/movie/${data.id}`, {state: {success: true}})
+        },
+        onError: () => {
+            setshowError(true)
+        }
+    })
+
+    useEffect(() => {
+        if(showError) {
+            setTimeout(() => {
+                setshowError(false)
+            }, 2000)
+        }
+    }, [showError])
+
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-        console.log(titleRef!.current!.value)
-        console.log(urlRef!.current!.value)
-        console.log(descriptionRef!.current!.value)
-        console.log(releaseMonthRef!.current!.value)
-        console.log(releaseYearRef!.current!.value)
+
+        mutate({
+            title: titleRef!.current!.value,
+            imageUrl: urlRef!.current!.value,
+            description: descriptionRef!.current!.value,
+            releaseDate: `${releaseMonthRef!.current!.value} ${releaseYearRef!.current!.value}`,
+            id: `${Date.now()}`
+        })
+
     }
 
     return (
         <>
             <h2>Add movie</h2>
+            {showError && <MessageAlert type='error' message={`${error}`}/>}
             <form onSubmit={handleSubmit} className={styles.form}>
-                <Input label="Name" type="text" ref={titleRef} required={true}/>
-                <Input label="Image URL" type="url" ref={urlRef} required={true}/>
+                <Input label="Name" type="text" ref={titleRef} required={true} />
+                <Input label="Image URL" type="url" ref={urlRef} required={true} />
                 <Textarea rows={10} label='Description' ref={descriptionRef} />
                 <div className={styles.releaseDateContainer}>
-                    <MonthSelect ref={releaseMonthRef}/>
-                    <Input label='year' type='number' ref={releaseYearRef} required={true}/>
+                    <MonthSelect ref={releaseMonthRef} />
+                    <Input label='year' type='number' ref={releaseYearRef} required={true} />
                 </div>
                 <div className={styles.buttonsContainer}>
                     <Button danger={true}>Cancel</Button>
--- src/service/fecthMovies.ts
+++ src/service/fecthMovies.ts
@@ -3,18 +3,32 @@ import { Movie } from "../movieInterface"
 export async function getMovies(): Promise<Movie[]> {
     const api = await fetch('http://localhost:3000/movies')
 
-    if(!api.ok) {
-        throw new Error (`Error: ${api.status}`)
+    if (!api.ok) {
+        throw new Error(`Error: ${api.status}`)
     }
 
-    return api.json() 
+    return api.json()
 }
 
 export async function getMovie(id: string): Promise<Movie> {
     const api = await fetch(`http://localhost:3000/movies/${id}`)
 
-    if(!api.ok) {
-        throw new Error (`${api.status}`)
+    if (!api.ok) {
+        throw new Error(`${api.status}`)
+    }
+
+    return api.json()
+}
+
+export async function postMovie(newMovie: Movie) {
+    const api = await fetch(`http://localhost:3000/movies`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newMovie)
+    })
+
+    if (!api.ok) {
+        throw new Error(`${api.status}`)
     }
 
     return api.json()
