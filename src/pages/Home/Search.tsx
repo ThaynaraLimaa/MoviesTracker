63d1996b7b6b55518e2f1bccb82ac771bff9e092
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2024-11-18 14:09:25 -0300

    feat: add state handling in Search component

--- src/pages/Home/Search.tsx
+++ src/pages/Home/Search.tsx
@@ -1,12 +1,34 @@
 import styles from './Search.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction, useState } from 'react'
 
-export default function Search() {
+interface SearchProps {
+    setSearchTerm: Dispatch<SetStateAction<string | undefined>>
+}
+
+export default function Search({ setSearchTerm }: SearchProps) {
+    const [searchText, setSearchText] = useState('')
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        setSearchTerm(searchText)
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value)
+        if (e.target.value == '') {
+            setSearchTerm('')
+        }
+    }
+
     return (
-        <form className={styles.searchContainer}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.searchIcon}/>
-            <input type="search" className={styles.search} placeholder='Search' required disabled/>
-        </form>
+        <>
+            <form className={styles.searchContainer} onSubmit={handleSubmit}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.searchIcon} />
+                <input type="search" className={styles.search} placeholder='Search' value={searchText} required
+                    onChange={handleChange} />
+            </form>
+        </>
     )
 }
