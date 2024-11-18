b7b397362fd397ba68a8900ebf169432266e64c5
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2024-11-18 14:24:48 -0300

    chore: remove data fetching from MovieList

--- src/pages/Home/MoviesList.tsx
+++ src/pages/Home/MoviesList.tsx
@@ -1,43 +1,24 @@
 import styles from './MoviesList.module.css'
-import { useQuery } from '@tanstack/react-query'
-import { getMovies } from '../../service/fecthMovies'
 import MovieCard from './MovieCard'
-import ErrorMessage from '../../components/UI/ErrorMessage'
-import LinkButton from '../../components/UI/LinkButton'
+import { Movie } from '../../movieInterface'
 
-export default function MoviesList() {
-    const { data: movies, error, isLoading, isError } = useQuery({
-        queryKey: ['movies'],
-        queryFn: getMovies
-    })
+interface MoviesListProps {
+    movies: Movie[],
+}
 
-    if (isLoading) return <h2>Loading...</h2>
-    if (isError) {
-        return (
-            <ErrorMessage name={error.name} message={error.message} />
-        )
-    }
-
+export default function MoviesList({ movies }: MoviesListProps) {
     return (
-
         <>
-            {movies!.length >= 1
-                ? <div className={styles.movieListContainer}>
-                    {movies!.map(movie =>
-                        <MovieCard
-                            key={movie.id}
-                            id={movie.id}
-                            title={movie.title}
-                            url={movie.imageUrl}
-                        />
-                    )}
-                </div>
-                : <div className={styles.noMovieContainer}>
-                    <h2>No mvoies in here..</h2>
-                    <p>Let's add a new movie to your collection!</p>
-                    <LinkButton to='/addMovie'>Add movie</LinkButton>
-                </div>
-            }
+            <div className={styles.movieListContainer}>
+                {movies!.map(movie =>
+                    <MovieCard
+                        key={movie.id}
+                        id={movie.id}
+                        title={movie.title}
+                        url={movie.imageUrl}
+                    />
+                )}
+            </div>
         </>
     )
 }
