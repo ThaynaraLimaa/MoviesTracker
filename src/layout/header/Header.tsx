be5d4a314760cd23e9f570056e9dfe4d9deda56c
thaynaralimaa61@gmail.com <thaynaralimaa61@gmail.com>
2025-04-01 10:13:13 -0300

    update header style

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -5,10 +5,6 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <Nav />
-            <div className={styles.titleWrapper}>
-                <h1 className={styles.headerTitle}>Movies Tracker</h1>
-                <p>All your movies in one place.</p>
-            </div>
         </header>
     )
 }
